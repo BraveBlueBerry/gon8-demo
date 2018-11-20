f51d5ac50595a864417e777e0a495e2dcb5e0c9d
Talitha Voortman <t.g.i.voortman@st.hanze.nl>
2018-11-20 14:18:20 +0100

    Main menu and level 1

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    [SerializeField]
+    string scene;
+    SceneManager scManager;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void StartLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void StartMainMenu()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void LoadStringScene()
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
